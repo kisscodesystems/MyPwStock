3a1a531ad8a89eed14ce0340449d2b6d9bd86778
Jozsef Kiss <info@kisscodesystems.com>
2026-07-05 07:50:01 +0200

    Add files via upload

--- MyPwStock_build.sh
+++ MyPwStock_build.sh
@@ -6,7 +6,7 @@ javac -d bin src/com/kisscodesystems/MyPwStock/*.java
 # 2. Package a runnable jar using the bundled manifest (it sets Main-Class).
 cd bin && jar cvfm MyPwStock.jar ../src/com/kisscodesystems/MyPwStock/manifest.txt com/kisscodesystems/MyPwStock/*.class
 
-mv MyPwStock.jar ../
+cp MyPwStock.jar ../
 
 echo ""
 echo "You can now start your application by"
